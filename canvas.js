721792ffdaf4c058c80505035ff3d832b9535d42
alexSp84 <alex1984@hotmail.it>
2018-11-02 18:04:07 +0100

    add more objects of the same shape

--- canvas.js
+++ canvas.js
@@ -1,50 +1,47 @@
 var app = new PIXI.Application(1200, 1000, {antialias: true, backgroundColor: 0xFFFFFF});
 document.getElementById("canvas").appendChild(app.view);
 
-var squareObj = new PIXI.Graphics();
-var rectangleObj = new PIXI.Graphics();
-var circleObj = new PIXI.Graphics();
-var triangleObj = new PIXI.Graphics();
-
-squareObj.interactive = true;
-squareObj.lineStyle(4, 0x000000);
-rectangleObj.interactive = true;
-rectangleObj.lineStyle(4, 0x000000);
-circleObj.interactive = true;
-circleObj.lineStyle(4, 0x000000);
-triangleObj.interactive = true;
-triangleObj.lineStyle(4, 0x000000);
-
-move(squareObj);
-move(rectangleObj);
-move(circleObj);
-move(triangleObj);
-
 function square() {
+    var squareObj = new PIXI.Graphics();
+    squareObj.interactive = true;
+    squareObj.lineStyle(4, 0x000000);
     squareObj.drawRect(0, 0, 200, 200);
     squareObj.hitArea = new PIXI.Rectangle(0, 0, 200, 200);
     app.stage.addChild(squareObj);
+    move(squareObj);
 }
 
 function rectangle() {
+    var rectangleObj = new PIXI.Graphics();
+    rectangleObj.interactive = true;
+    rectangleObj.lineStyle(4, 0x000000);
     rectangleObj.drawRect(0, 0, 300, 100);
     rectangleObj.hitArea = new PIXI.Rectangle(0, 0, 300, 100);
     app.stage.addChild(rectangleObj);
+    move(rectangleObj);
 }
 
 function circle() {
+    var circleObj = new PIXI.Graphics();
+    circleObj.interactive = true;
+    circleObj.lineStyle(4, 0x000000);
     circleObj.hitArea = new PIXI.Circle(50, 50, 50);
     circleObj.drawCircle(50, 50, 50);
     app.stage.addChild(circleObj);
+    move(circleObj);
 }
 
 function triangle() {
+    var triangleObj = new PIXI.Graphics();
+    triangleObj.interactive = true;
+    triangleObj.lineStyle(4, 0x000000);
     triangleObj.hitArea = new PIXI.Rectangle(0, 0, 200, 200);
     triangleObj.moveTo(0, 0);
     triangleObj.lineTo(0, 200);
     triangleObj.lineTo(200, 200);
     triangleObj.lineTo(0, 0);
     app.stage.addChild(triangleObj);
+    move(triangleObj);
 }
 
 // setup events
