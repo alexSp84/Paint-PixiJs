008e1e08c951c7f608396cfedcccf7620c6bbdd1
alexSp84 <alex1984@hotmail.it>
2018-11-02 13:47:53 +0100

    drag and drop

--- canvas.js
+++ canvas.js
@@ -5,43 +5,27 @@ var graphics = new PIXI.Graphics();
 
 graphics.interactive = true;
 graphics.hitArea = new PIXI.Rectangle(0, 0, 1200, 1000);
-
+graphics.lineStyle(4, 0x000000);
 
 function square() {
-    graphics.click = function () {
-        console.log("click");
-        graphics.lineStyle(4, 0x000000);
-        graphics.drawRect(200, 100, 200, 200);
-    };
+    graphics.drawRect(200, 100, 200, 200);
     app.stage.addChild(graphics);
 }
 
 function rectangle() {
-    graphics.click = function () {
-        console.log("click");
-        graphics.lineStyle(4, 0x000000);
-        graphics.drawRect(200, 400, 300, 100);
-    };
+    graphics.drawRect(200, 400, 300, 100);
     app.stage.addChild(graphics);
 }
 
 function circle() {
-    graphics.click = function () {
-        console.log("click");
-        graphics.lineStyle(4, 0x000000);
-        graphics.drawCircle(500, 200, 50);
-    };
+    graphics.drawCircle(500, 200, 50);
     app.stage.addChild(graphics);
 }
 
 function triangle() {
-    graphics.click = function () {
-        console.log("click");
-        graphics.lineStyle(4, 0x000000);
-        graphics.moveTo(700, 600);
-        graphics.lineTo(700, 800);
-        graphics.lineTo(900, 800);
-        graphics.lineTo(700, 600);
-    };
+    graphics.moveTo(700, 600);
+    graphics.lineTo(700, 800);
+    graphics.lineTo(900, 800);
+    graphics.lineTo(700, 600);
     app.stage.addChild(graphics);
 }
